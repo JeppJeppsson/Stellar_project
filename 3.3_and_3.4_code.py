b33be24914e605376b6e5cac9cd5a617da838cdd
GluonFrog <skrepkonto2021@gmail.com>
2025-02-28 14:32:49 +0100

    Update to 3.3 code

--- 3.3_and_3.4_code.py
+++ 3.3_and_3.4_code.py
@@ -45,16 +45,16 @@ def plot_lum_values(data_files, data_dir):
         
         #Plotting the luminosity values normally 
         axs[0,i].plot(df['t'] / 1e9, df['Lpp'], label = r'$L_{\mathrm{pp}}$')
-        axs[0,i].plot(df['t'] / 1e9, df['LCNO'], label = r'$L_{\mathrm{CNO}}$')
-        axs[0,i].plot(df['t'] / 1e9, df['L3a'], color = 'g', label = r'$L_{3\alpha}$')
-        axs[0,i].set_ylabel(r"$L/L_\odot$")
+        axs[0,i].plot(df['t'] / 1e9, df['LCNO'], linestyle = '--', label = r'$L_{\mathrm{CNO}}$')
+        axs[0,i].plot(df['t'] / 1e9, df['L3a'], linestyle = '-.', color = 'g', label = r'$L_{3\alpha}$')
+        axs[0,i].set_ylabel(r"$L/$L$_\odot$")
         #axs[0,i].set_xlabel(r"$t \ [\mathrm{10^9 \ yr}]$")
         
         #Plotting on a log-scale for readability and comparison
         axs[1,i].semilogy(df['t'] / 1e9, df['Lpp'], label = r'$L_{\mathrm{pp}}$')
-        axs[1,i].semilogy(df['t'] / 1e9, df['LCNO'], label = r'$L_{\mathrm{CNO}}$')
-        axs[1,i].semilogy(df['t'] / 1e9, df['L3a'], color = 'g', label = r'$L_{3\alpha}$')
-        axs[1,i].set_ylabel(r"$L/L_\odot$")
+        axs[1,i].semilogy(df['t'] / 1e9, df['LCNO'], linestyle = '--', label = r'$L_{\mathrm{CNO}}$')
+        axs[1,i].semilogy(df['t'] / 1e9, df['L3a'], linestyle = '-.', color = 'g', label = r'$L_{3\alpha}$')
+        axs[1,i].set_ylabel(r"$L/$L$_\odot$")
         axs[1,i].set_xlabel(r"$t \ [\mathrm{10^9 \ yr}]$")
         
         
@@ -75,8 +75,12 @@ plot_lum_values(data_name, DATA_DIR)
 
 #The proportion of luminosity contribution
 
-def plot_lum_prop(data_files, data_dir):
+def plot_lum_prop(data_files, data_dir, zoom = False):
     fig, axs = plt.subplots(2)
+    
+    if zoom == True:
+        axs[0].set_xlim(1.38, 1.388)
+        axs[1].set_xlim(0.005, 0.0062)
     
     for i, data_name in enumerate(data_files):
         df = pd.read_csv(data_dir + data_name, sep=r'\s+', header=None)
@@ -85,28 +89,30 @@ def plot_lum_prop(data_files, data_dir):
         
         #Normalising values at each time-step
         for j in range(len(df['Lpp'])):
-            tot  = df['Lpp'][j] + df['LCNO'][j] + df['L3a'][j]
-            df.loc[j, "Lpp"] = df.loc[j, "Lpp"]/tot
-            df.loc[j,'LCNO'] = df.loc[j,'LCNO']/tot
-            df.loc[j,'L3a'] = df.loc[j,'L3a']/tot
+            #tot  = df.loc[j,'LogL']#
+            tot = df['Lpp'][j] + df['LCNO'][j] + df['L3a'][j]
+            df.loc[j, "Lpp"] = df.loc[j, "Lpp"]/tot#np.exp(tot)
+            df.loc[j,'LCNO'] = df.loc[j,'LCNO']/tot#np.exp(tot)
+            df.loc[j,'L3a'] = df.loc[j,'L3a']/tot#np.exp(tot)
             
-        print(i)
+        
         axs[i].plot(df['t'] / 1e9, df['Lpp'], label = r'$L_{\mathrm{pp}}$')
         
-        axs[i].plot(df['t'] / 1e9, df['LCNO'], label = r'$L_{\mathrm{CNO}}$')
+        axs[i].plot(df['t'] / 1e9, df['LCNO'], linestyle = '--', label = r'$L_{\mathrm{CNO}}$')
         
-        axs[i].plot(df['t'] / 1e9, df['L3a'], linestyle = '--', label = r'$L_{3\alpha}$')
+        axs[i].plot(df['t'] / 1e9, df['L3a'], linestyle = '-.', label = r'$L_{3\alpha}$')
         
         
-        axs[i].set_ylabel(r"Fraction of total L")
-        axs[i].set_xlabel(r"$t \ [\mathrm{10^9 \ yr}]$")
-    
-    
+        axs[i].set_ylabel(r"Fraction of total $L$")
+        axs[1].set_xlabel(r"$t \ [\mathrm{10^9 \ yr}]$")
+        
+        
     
         axs[i].set_title(plot_title[i])
         axs[i].grid(which='both', alpha=0.4, visible=True)
     fig.legend([r'$L_{\mathrm{pp}}$',r'$L_{\mathrm{CNO}}$',r'$L_{3\alpha}$'], bbox_to_anchor=(0.75, 0.05), ncol=3)
     plt.suptitle('Proportion of the luminosity contribution')
+    
     plt.tight_layout()
     plt.show()
 
@@ -118,9 +124,13 @@ plot_lum_prop(data_name, DATA_DIR)
 
 #Data for power per unit mass
 
+#time_steps_2 = [10, 380, 550, 830]
+time_steps_2 = [10, 380, 580, 730, 920, 950]
+time_steps_30 = [10, 140, 319, 524]
+
 data_names = ['2msun_02_SI','30msun_02_SI']
 
-def get_structure_files(data_dir, time_frac):
+def get_structure_files(data_dir, time_steps):
     df = pd.read_csv(data_dir + '/summary.txt', sep=r'\s+', header=None)
     df.columns = col_names
     all_files = os.listdir(os.path.join(data_dir))
@@ -131,12 +141,12 @@ def get_structure_files(data_dir, time_frac):
     df_ref.columns = col_names_structure
     
     # Find indices closest to given time fractions
-    time_frac_idx = [np.argmin(np.abs(df['t'] - (t * np.max(df['t'])))) for t in time_frac]
+    #time_frac_idx = [np.argmin(np.abs(df['t'] - (t * np.max(df['t'])))) for t in time_frac]
     
-    formatted_indices = [str(idx).zfill(5) for idx in time_frac_idx]
+    formatted_indices = [str(idx).zfill(5) for idx in time_steps]
     filtered_files = [f for f in structure_files if f[10:15] in formatted_indices]
     
-    t = [df['t'].values[idx] for idx in time_frac_idx]
+    t = [df['t'].values[idx] for idx in time_steps]
     
     return sorted(filtered_files), t
 
@@ -149,7 +159,8 @@ def get_max_num(data_dir):
     return max_time_step
 
 def plot_rad_contr(data_files, data_dir, max_iter, name, time):
-
+    fig, axs = plt.subplots(len(data_files), sharex= True)
+    
     cmap = cm.copper
     norm = Normalize(vmin=0, vmax=1)
     for i,data_name in enumerate(data_files):
@@ -161,31 +172,29 @@ def plot_rad_contr(data_files, data_dir, max_iter, name, time):
         
         r = df['r']
         
-        time_percentage = int(data_name[10:15])/max_iter 
-        color = cmap(norm(time_percentage)) 
+        axs[i].semilogx(r, df['epspp'], linestyle = '-', label = 'pp-chain')
+        axs[2].set_ylabel(r"Power per unit mass [$\mathrm{Wkg^{-1}}$]")
         
-        plt.semilogx(r, df['epspp'], color = color, linestyle = '-', label = 'pp-chain')
-        plt.ylabel(r"Power per unit mass [$\mathrm{W/kg}$]")
-        
-        plt.semilogx(r, df['epscno'], color = color, linestyle = '--', label = 'CNO-cycle')
+        axs[i].semilogx(r, df['epscno'], linestyle = '--', label = 'CNO-cycle')
         
         
-        plt.semilogx(r, df['eps3alpha'], color = color, linestyle = '-.', label = r'$3\alpha$-process')
-        plt.xlabel(r"$r/R_\odot$ [1]")
+        axs[i].semilogx(r, df['eps3alpha'], linestyle = '-.', label = r'$3\alpha$-process')
+        axs[len(data_files)-1].set_xlabel(r"$r$/R$_\odot$ [1]")
         #plt.colorbar(label =r'$t/t_\mathrm{end}$', aspect = 0.8)
-        plt.legend()
         
-        plt.title(name + rf'star at {time[i]/1e6:.2f} $\cdot$ $10^6$ yr')
-        plt.show()
+        axs[i].annotate( rf'{time[i]/1e6:.2f} $\cdot$ $10^6$ yr', xy=(.71, .5), xycoords='axes fraction',bbox=dict(facecolor='none', edgecolor='grey'))
+    axs[1].legend(bbox_to_anchor=(0.3, 0.3), fontsize = 4)
+    plt.suptitle(name + rf'star')
+    plt.subplots_adjust(hspace=0)#, wspace=0)
+    plt.show()
 
-
-time_frac =  [1e-9, 0.55, 0.75, 0.9, 0.95, 1]
         
 for i, name in enumerate(data_names):
+    steps = [time_steps_2,time_steps_30]
     full_path = DATA_DIR + name
     max_iter = get_max_num(full_path)
-    data_files = get_structure_files(full_path, time_frac)[0]
-    time = get_structure_files(full_path, time_frac)[1]
+    data_files = get_structure_files(full_path, steps[i])[0]
+    time = get_structure_files(full_path, steps[i])[1]
     plot_rad_contr(data_files, full_path, max_iter, plot_title[i], time)
     
     
